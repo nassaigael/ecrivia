5f8345980651afb66b13877aa6cd281d390108b7
nassaigael <hei.nassai@gmail.com>
2026-01-19 15:57:55 +0300

    Remove unused EmailWriterApp and AuthComponent components to streamline the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import EmailComposerApp from './pages/EmailComposerApp'
-import EmailWriterApp from './pages/EmailWriterApp'
 
 function App() {
 
   return (
     <>
         {/* <EmailWriterApp/> */}
-        {/* <EmailComposerApp/> */}
         <EmailComposerApp/>
     </>
   )
